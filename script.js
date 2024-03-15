0c861b69972df882b6d158db22e5446165523a19
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2024-03-15 10:57:19 -0400

    owo

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", async function () {
   //   carousel(carouselText, "#feature-text");
 });
 
-async function typeSentence(sentence, eleRef, minDelay = 30, maxDelay = 100) {
+async function typeSentence(sentence, eleRef, minDelay = 20, maxDelay = 70) {
   const letters = sentence.split("");
   let i = 0;
   while (i < letters.length) {
@@ -66,6 +66,86 @@ window.onload = function () {
     setTimeout(function () {
       socket.remove(); // Remove the socket element after the transition completes
       document.querySelector("main").classList.remove("gone");
-    }, 100); // Wait for 1 second, which is the same duration as the CSS transition
-  }, 100);
+    }, 1000); // Wait for 1 second, which is the same duration as the CSS transition
+  }, 1000);
 };
+
+var w = window.innerWidth,
+  h = window.innerHeight,
+  canvas = document.getElementById("test"),
+  ctx = canvas.getContext("2d"),
+  rate = 60,
+  arc = 100,
+  time,
+  count,
+  size = 7,
+  speed = 20,
+  parts = new Array(),
+  colors = ["#eb6f92", "#f6c177", "#ebbcba", "#9ccfd8", "#c4a7e7"];
+var mouse = { x: 0, y: 0 };
+
+canvas.setAttribute("width", w);
+canvas.setAttribute("height", h);
+
+function create() {
+  time = 0;
+  count = 0;
+
+  for (var i = 0; i < arc; i++) {
+    parts[i] = {
+      x: Math.ceil(Math.random() * w),
+      y: Math.ceil(Math.random() * h),
+      toX: Math.random() * 5 - 1,
+      toY: Math.random() * 2 - 1,
+      c: colors[Math.floor(Math.random() * colors.length)],
+      size: Math.random() * size,
+    };
+  }
+}
+
+function particles() {
+  ctx.clearRect(0, 0, w, h);
+  canvas.addEventListener("mousemove", MouseMove, false);
+  for (var i = 0; i < arc; i++) {
+    var li = parts[i];
+    var distanceFactor = DistanceBetween(mouse, parts[i]);
+    var distanceFactor = Math.max(Math.min(15 - distanceFactor / 10, 10), 1);
+    ctx.beginPath();
+    ctx.arc(li.x, li.y, li.size * distanceFactor, 0, Math.PI * 2, false);
+    ctx.fillStyle = li.c;
+    ctx.strokeStyle = li.c;
+    if (i % 2 == 0) ctx.stroke();
+    else ctx.fill();
+
+    li.x = li.x + li.toX * (time * 0.05);
+    li.y = li.y + li.toY * (time * 0.05);
+
+    if (li.x > w) {
+      li.x = 0;
+    }
+    if (li.y > h) {
+      li.y = 0;
+    }
+    if (li.x < 0) {
+      li.x = w;
+    }
+    if (li.y < 0) {
+      li.y = h;
+    }
+  }
+  if (time < speed) {
+    time++;
+  }
+  setTimeout(particles, 1000 / rate);
+}
+function MouseMove(e) {
+  mouse.x = e.layerX;
+  mouse.y = e.layerY;
+}
+function DistanceBetween(p1, p2) {
+  var dx = p2.x - p1.x;
+  var dy = p2.y - p1.y;
+  return Math.sqrt(dx * dx + dy * dy);
+}
+create();
+particles();
