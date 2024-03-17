d4c77509473279acadb18f4d856d8da0c5815c8f
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2024-03-16 21:29:49 -0400

    w

---
+++ particles.js
@@ -0,0 +1,125 @@
+var canvas = document.querySelector("#scene"),
+  ctx = canvas.getContext("2d"),
+  particles = [],
+  amount = 0,
+  mouse = { x: 0, y: 0 },
+  radius = 0;
+
+var colors = ["#e0def4"];
+
+var textToRender = "justanotherinternetguy";
+
+class Particle {
+  constructor(x, y, canvasWidth, canvasHeight) {
+    this.x = Math.random() * canvasWidth;
+    this.y = Math.random() * canvasHeight;
+    this.dest = {
+      x: x,
+      y: y,
+    };
+    this.r = Math.random() * (canvasWidth / 300) + 1;
+    this.vx = (Math.random() - 0.5) * 20;
+    this.vy = (Math.random() - 0.5) * 20;
+    this.accX = 0;
+    this.accY = 0;
+    this.friction = Math.random() * 0.05 + 0.91;
+
+    this.color = colors[Math.floor(Math.random() * colors.length)];
+
+    this.Kp = 0.006;
+    this.Ki = 0.00001;
+    this.Kd = 0.05;
+    this.prevErrorX = 0;
+    this.prevErrorY = 0;
+    this.integralX = 0;
+    this.integralY = 0;
+  }
+
+  render() {
+    let errorX = this.dest.x - this.x;
+    let errorY = this.dest.y - this.y;
+
+    this.integralX += errorX;
+    this.integralY += errorY;
+
+    this.accX =
+      this.Kp * errorX +
+      this.Ki * this.integralX +
+      this.Kd * (errorX - this.prevErrorX);
+    this.accY =
+      this.Kp * errorY +
+      this.Ki * this.integralY +
+      this.Kd * (errorY - this.prevErrorY);
+
+    this.prevErrorX = errorX;
+    this.prevErrorY = errorY;
+
+    this.accX *= this.friction;
+    this.accY *= this.friction;
+
+    this.vx += this.accX;
+    this.vy += this.accY;
+
+    this.x += this.vx;
+    this.y += this.vy;
+
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.r, Math.PI * 2, false);
+    ctx.fill();
+  }
+}
+
+function initScene() {
+  var computedStyle = getComputedStyle(canvas);
+  var canvasWidth = parseInt(computedStyle.width);
+  var canvasHeight = parseInt(computedStyle.height);
+
+  canvas.width = canvasWidth;
+  canvas.height = canvasHeight;
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ctx.font = "bold " + canvasWidth / 14 + "px monospace";
+  ctx.textAlign = "center";
+  ctx.fillText(textToRender, canvasWidth / 2, canvasHeight / 2);
+
+  var data = ctx.getImageData(0, 0, canvasWidth, canvasHeight).data;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.globalCompositeOperation = "screen";
+
+  particles = [];
+  for (var i = 0; i < canvasWidth; i += Math.round(canvasWidth / 300)) {
+    for (var j = 0; j < canvasHeight; j += Math.round(canvasWidth / 300)) {
+      if (data[(i + j * canvasWidth) * 4 + 3] > 150) {
+        particles.push(new Particle(i, j, canvasWidth, canvasHeight));
+      }
+    }
+  }
+
+  amount = particles.length;
+}
+
+function render(a) {
+  requestAnimationFrame(render);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (var i = 0; i < amount; i++) {
+    particles[i].render();
+  }
+}
+
+// window.addEventListener("resize", initScene);
+
+// initScene();
+// requestAnimationFrame(render);
+
+function startAnimation() {
+  setTimeout(function () {
+    requestAnimationFrame(render);
+  }, 1600);
+}
+
+window.addEventListener("resize", initScene);
+
+initScene();
+startAnimation();
--- script.js
+++ script.js
@@ -5,11 +5,14 @@ const carouselText = [
 ];
 
 document.addEventListener("DOMContentLoaded", async function () {
-  typeSentence(
-    "welcome to justanotherinternetguy's corner of the www",
-    "#sentence"
-  );
-  //   carousel(carouselText, "#feature-text");
+  setTimeout(async function () {
+    await typeSentence(
+      "welcome to justanotherinternetguy's corner of the www",
+      "#sentence"
+    );
+    // Start carousel after typing the welcome message
+    // carousel(carouselText, "#feature-text");
+  }, 1600);
 });
 
 async function typeSentence(sentence, eleRef, minDelay = 20, maxDelay = 70) {
@@ -65,88 +68,9 @@ window.onload = function () {
     socket.style.opacity = 0; // Set opacity to 0 for transition
     setTimeout(function () {
       socket.remove(); // Remove the socket element after the transition completes
+      document.querySelector("canvas").classList.remove("gone");
+      document.querySelector("header").classList.remove("gone");
       document.querySelector("main").classList.remove("gone");
-    }, 100); // Wait for 1 second, which is the same duration as the CSS transition
-  }, 100);
+    }, 1000); // Wait for 1 second, which is the same duration as the CSS transition
+  }, 1000);
 };
-
-var w = window.innerWidth,
-  h = window.innerHeight,
-  canvas = document.getElementById("test"),
-  ctx = canvas.getContext("2d"),
-  rate = 60,
-  arc = 100,
-  time,
-  count,
-  size = 8,
-  speed = 5,
-  parts = new Array(),
-  colors = ["#eb6f92", "#f6c177", "#ebbcba", "#9ccfd8", "#c4a7e7"];
-var mouse = { x: 0, y: 0 };
-
-canvas.setAttribute("width", w);
-canvas.setAttribute("height", h);
-
-function create() {
-  time = 0;
-  count = 0;
-
-  for (var i = 0; i < arc; i++) {
-    parts[i] = {
-      x: Math.ceil(Math.random() * w),
-      y: Math.ceil(Math.random() * h),
-      toX: Math.random() * 5 - 1,
-      toY: Math.random() * 2 - 1,
-      c: colors[Math.floor(Math.random() * colors.length)],
-      size: Math.random() * size,
-    };
-  }
-}
-
-function particles() {
-  ctx.clearRect(0, 0, w, h);
-  canvas.addEventListener("mousemove", MouseMove, false);
-  for (var i = 0; i < arc; i++) {
-    var li = parts[i];
-    var distanceFactor = DistanceBetween(mouse, parts[i]);
-    var distanceFactor = Math.max(Math.min(15 - distanceFactor / 10, 10), 1);
-    var distanceFactor = 1.5;
-    ctx.beginPath();
-    ctx.arc(li.x, li.y, li.size * distanceFactor, 0, Math.PI * 2, false);
-    ctx.fillStyle = li.c;
-    ctx.strokeStyle = li.c;
-    if (i % 2 == 0) ctx.stroke();
-    else ctx.fill();
-
-    li.x = li.x + li.toX * (time * 0.05);
-    li.y = li.y + li.toY * (time * 0.05);
-
-    if (li.x > w) {
-      li.x = 0;
-    }
-    if (li.y > h) {
-      li.y = 0;
-    }
-    if (li.x < 0) {
-      li.x = w;
-    }
-    if (li.y < 0) {
-      li.y = h;
-    }
-  }
-  if (time < speed) {
-    time++;
-  }
-  setTimeout(particles, 1000 / rate);
-}
-function MouseMove(e) {
-  mouse.x = e.layerX;
-  mouse.y = e.layerY;
-}
-function DistanceBetween(p1, p2) {
-  var dx = p2.x - p1.x;
-  var dy = p2.y - p1.y;
-  return Math.sqrt(dx * dx + dy * dy);
-}
-create();
-particles();
